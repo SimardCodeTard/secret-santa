bf0e556501092a277c9703913b8e2009eaa43a6b
Simard <floogles71@gmail.com>
2024-11-05 03:39:24 +0100

    remove default export

--- app/api/v2/draws/[userId]/route.ts
+++ app/api/v2/draws/[userId]/route.ts
@@ -2,7 +2,7 @@ import { CollectionNamesEnum, findMany, getCollection } from "@/app/lib/mongo";
 import { ObjectId } from "mongodb";
 import { NextRequest } from "next/server";
 
-export default async function GET(_: NextRequest, params: { userId: string }) {
+export async function GET(_: NextRequest, params: { userId: string }) {
     const userId = new ObjectId(params.userId);
     
     const collection = await getCollection(CollectionNamesEnum.DRAW);
