545b47a828603b44da2cec9c958ffb0f3452bb7e
Simard <floogles71@gmail.com>
2024-11-05 02:57:50 +0100

    login route

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css';
+
+export default function WhodisPage() {
+    return <div className={styles.page}>
+        <main className={styles.main}>
+
+        </main>
+    </div>
+}
